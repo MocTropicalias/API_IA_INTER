2eb8a0a3674154beb62facc4fec1a8a0ee8bea4d
FelipeRRMiotto <felipe.miotto@germinare.org.br>
2024-11-05 09:17:23 -0300

    desbloqueando Cross Operation e adicionando feture para salvar dados da requisição no banco

--- API_IA.py
+++ API_IA.py
@@ -2,8 +2,12 @@ from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
 import os
+from flask_cors import CORS
+import psycopg as pg
 
+banco = "postgres://avnadmin:AVNS_69W0O2_65jEqbsCuztW@pg-11d01e0e-testepgsql.e.aivencloud.com:24931/dbBaseIA?sslmode=require"
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/process', methods=['POST'])
 def process_json():
@@ -24,7 +28,8 @@ def process_json():
     for key in required_keys:
         if key not in data:
             return jsonify({"error": f"Campo obrigatório ausente: {key}"}), 400
-
+        
+    query = "insert into tb_base(var_genero, int_idade, var_renda, var_estado, var_eletronicos, var_educacao, var_esportes, var_locais_publicos, var_result) values('"+data["Qual é seu genero?"]+"',"+data["Qual é a sua idade?"]+",'"+data["Qual é a média da sua renda familiar mensal?"]+"','"+data["Qual estado você mora?"]+"','"+data["Você usa muitos eletrônicos durante o dia? (6h ou mais)"]+"','"+data["Qual grau de educação você tem?"]+"','"+data["Você pratica esportes? (pelo menos 3 vezes na semana)"]+"','"+data["Você frequenta muito espaços públicos? (parques',' museus e etc)"]+"','"
 
     caminho_arq_tratamento = os.path.dirname(__file__)+"/pkls/tratamento_dados.pkl"
 
@@ -43,11 +48,25 @@ def process_json():
     x = pd.DataFrame(tratamento_dados.transform(dados),columns=tratamento_dados.get_feature_names_out())
 
     previsao = modelo.predict(x)
-
-    if previsao == 1:
-        return jsonify({"result": True})
-    else:
-        return jsonify({"result": False})
+    query = query + str(previsao)+"')"
+    try:
+        conn = pg.connect(banco)
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+    except (pg.Error) as error:
+        print("Erro banco: ", error)
+    except (Exception) as error:
+        print("Erro código python")
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+        print("Conexões encerradas\n\n")
+        if previsao == 1:
+            return jsonify({"result": True})
+        else:
+            return jsonify({"result": False})
 
 def run_python_code(data):
     return True
